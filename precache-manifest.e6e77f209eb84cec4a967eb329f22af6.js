3bc2475d1df20d3ba1af5210ca9d2dde7365f8c4
hgupta10 <himanshu.h.gupta@capgemini.com>
2019-11-03 04:24:23 +0530

    Updates

---
+++ precache-manifest.e6e77f209eb84cec4a967eb329f22af6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113ed82c33c508271bfddc16d076a86c",
+    "url": "/twitter_search_with_redux/index.html"
+  },
+  {
+    "revision": "b75e8c6e14f25ecab953",
+    "url": "/twitter_search_with_redux/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "feba9b4b94ce6323e41a",
+    "url": "/twitter_search_with_redux/static/css/main.a0cfc7e2.chunk.css"
+  },
+  {
+    "revision": "b75e8c6e14f25ecab953",
+    "url": "/twitter_search_with_redux/static/js/2.8ed68fd0.chunk.js"
+  },
+  {
+    "revision": "feba9b4b94ce6323e41a",
+    "url": "/twitter_search_with_redux/static/js/main.b2d72578.chunk.js"
+  },
+  {
+    "revision": "318842780ddd6011e283",
+    "url": "/twitter_search_with_redux/static/js/runtime~main.1bce18db.js"
+  },
+  {
+    "revision": "a57ea219084451288a1f02aabba16f1b",
+    "url": "/twitter_search_with_redux/static/media/twitter.a57ea219.svg"
+  }
+]);
